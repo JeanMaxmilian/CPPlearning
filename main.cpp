1f2f70d2e18d713d90b3121a7ff1b3a58dc0fc2d
JeanMaxmilian <jeanmaximilianisidro@gmail.com>
2023-05-31 19:12:52 +0530

    added a program to check compiler configurations.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// program to check whether the compileris configured correctly
+//terminal-> configure tasks
+//edit tasks.json set "-std=c++20" for c++ version 20
+//termial->new terminal
+//terminal->run task
+
+#include <iostream>
+
+int main(){
+
+    auto result  = (10 <=> 20) > 0;
+    std::cout<<result<<std::endl;
+    return 0;
+}
